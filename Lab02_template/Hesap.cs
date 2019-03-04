236f6f700ca71353ce73dfa663d7cc2f36471411
baristezel <baris.tezel@deu.edu.tr>
2019-03-04 10:52:08 +0300

    Add files via upload

---
+++ Lab02_template/Hesap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab02_template
+{
+    public struct Koordinat
+    {
+
+    }
+    public struct Daire
+    {
+
+    }
+    public struct Dikdortgen
+    {
+
+    }
+    public class Hesap
+    {
+    }
+}
---
+++ Lab02_template/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab02_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
